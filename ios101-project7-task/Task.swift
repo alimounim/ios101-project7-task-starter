5307d962494ea8d902c83d556af948da82e6f6fe
Ali Mounim Rajabi <129315884+alimounim@users.noreply.github.com>
2025-11-05 22:23:15 -0600

    save_update_add

--- ios101-project7-task/Task.swift
+++ ios101-project7-task/Task.swift
@@ -5,7 +5,7 @@
 import UIKit
 
 // The Task model
-struct Task {
+struct Task: Codable {
 
     // The task's title
     var title: String
@@ -58,19 +58,54 @@ extension Task {
     static func save(_ tasks: [Task]) {
 
         // TODO: Save the array of tasks
+        // Save an array of tasks to UserDefaults.
+        //    - we add the `static` keyword to make this a "Type Method".
+        //    - We can call it from anywhere by calling it on the `Task` type.
+        // 1. Create an instance of UserDefaults
+        let defaults = UserDefaults.standard
+        // 2. Try to encode the array of `Task` objects to `Data`
+        let encodedData = try? JSONEncoder().encode(tasks)
+        // 3. Save the encoded movie `Data` to UserDefaults
+        defaults.set(encodedData, forKey: "tasks")
+        // 🐞 DEBUG: Print movie titles to console to see which movies we just saved
+        print("🍿 ---Tasks---")
+        print(tasks.map(\.title).joined(separator: "\n"))
     }
 
     // Retrieve an array of saved tasks from UserDefaults.
     static func getTasks() -> [Task] {
         
         // TODO: Get the array of saved tasks from UserDefaults
-
-        return [] // 👈 replace with returned saved tasks
+        // 1. Create an instance of UserDefaults
+        let defaults = UserDefaults.standard
+        // 2. Get any tasks `Data` saved to UserDefaults (if any exist)
+        if let savedData = defaults.data(forKey: "tasks"){
+            // 3. Try to decode the task `Data` to `Task` objects
+            let decodedTask = try! JSONDecoder().decode([Task].self, from: savedData)
+            // 4. If 2-3 are successful, return the array of movies
+            return decodedTask
+        } else {
+            // 5. Otherwise, return an empty array
+            return [] // 👈 replace with returned saved tasks
+        }
     }
 
     // Add a new task or update an existing task with the current task.
     func save() {
 
         // TODO: Save the current task
+        // 1. Get all tasks from UserDefaults
+        //    - We make `tasks` a `var` so we'll be able to modify it when adding another task
+        var tasks = Task.getTasks()
+        // 2. Add the task to the tasks array
+        //   - Since this method is available on "instances" of a task, we can reference the task this method is being called on using `self`.
+        if let i = tasks.firstIndex(where: { $0.id == self.id }) {
+            tasks[i] = self      // update existing
+        } else {
+            tasks.append(self)   // add new
+        }
+        // 3. Save the updated tasks array
+        Task.save(tasks)
+        
     }
 }
